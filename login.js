aee684de59a1977705c19fa0b3f13c9f7f419563
Cezary Szczepaniak <u142_cezszc_waw@technischools.com>
2024-10-10 14:04:23 +0200

    sa

--- login.js
+++ login.js
@@ -1,4 +1,5 @@
 function logowanie() {
+  // Pobieranie danych z formularza
   let username = document.getElementById("loginn").value;
   let password = document.getElementById("passwordd").value;
   let errorMessage = document.getElementById("error-message"); // Element na komunikat o błędzie
@@ -9,24 +10,11 @@ function logowanie() {
   // Sprawdzanie hasła i loginu (przykład bez szyfrowania)
   if (username === "admin" && password === "DRAGON") {
     alert("Login successfully");
-    window.location = "https://technischools.com"; // Przekierowanie do innej strony.
+    window.location = "https://technischools.com"; // Przekierowanie do innej strony
     return false; // Zatrzymanie wysłania formularza
   } else {
     // Wyświetlenie komunikatu o błędnym haśle lub loginie
     errorMessage.textContent = "Incorrect username or password";
     return false; // Zatrzymanie wysłania formularza
-  }
-}
-
-function admin() {
-  let admin = document.getElementById("loginn").value;
-  let errorMessage = document.getElementById("error-message");
-
-  // Wyczyszczenie poprzedniego komunikatu o błędzie
-  errorMessage.textContent = "";
-
-  if (admin === "admin") {
-    errorMessage.textContent = "Użytkownik o nazwie admin istnieje";
-    return false;
   }
 }
